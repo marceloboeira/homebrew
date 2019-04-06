9e50184e26cab13fd72c25ee3f5b9503226e339a
Marcelo Boeira <me@marceloboeira.com>
2019-04-06 18:33:59 +0200

    Add first RC for awsudo

--- awsudo.rb
+++ awsudo.rb
@@ -1,9 +1,9 @@
 class Awsudo < Formula
   desc "sudo-like behavior for role assumed access on AWS accounts"
   homepage "https://github.com/marceloboeira/awsudo"
-  url "https://github.com/marceloboeira/awsudo/releases/download/0.0.1/awsudo-0.0.1.x86_64-apple-darwin.tar.gz"
-  version "0.0.1"
-  sha256 "e6c299626b916def5c28c5f5bece0b1fb58dd28d52b0596beb556a102f25e801"
+  url "https://github.com/marceloboeira/awsudo/releases/download/0.1.0/awsudo-0.1.0.x86_64-apple-darwin.tar.gz"
+  version "0.1.0"
+  sha256 "9723eee8e67a6e31770cfd20c2cb6b1210ebb206802a312be678b6364c1526fa"
 
   def install
     bin.install "awsudo"
