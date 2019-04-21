53778fc6b361efe465d48b375922c8ab3829d268
Marcelo Boeira <me@marceloboeira.com>
2019-04-21 11:05:24 +0200

    Update awsudo version

--- awsudo.rb
+++ awsudo.rb
@@ -1,9 +1,8 @@
 class Awsudo < Formula
   desc "sudo-like behavior for role assumed access on AWS accounts"
   homepage "https://github.com/marceloboeira/awsudo"
-  url "https://github.com/marceloboeira/awsudo/releases/download/0.1.0/awsudo-0.1.0.x86_64-apple-darwin.tar.gz"
-  version "0.1.0"
-  sha256 "9723eee8e67a6e31770cfd20c2cb6b1210ebb206802a312be678b6364c1526fa"
+  url "https://github.com/marceloboeira/awsudo/releases/download/0.1.1/awsudo-0.1.1.x86_64-apple-darwin.tar.gz"
+  sha256 "1dd84a1f74d877de8de1937d131f16f4f0a44bc3bb59d38894a5a0c3aeb76b62"
 
   def install
     bin.install "awsudo"
